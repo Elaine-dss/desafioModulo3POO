ce75b4fd63544f0e4b24b6cd4c17b9c94ab4c5e8
Elaine <elaine.silva@zup.com.br>
2021-10-10 16:57:47 -0300

    "criar metodo para capturar os dados do usuario"

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,4 +1,11 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
+
+    public static Scanner capturarDados(String msg) {
+        System.out.print(msg);
+        return new Scanner(System.in);
+    }
 }
