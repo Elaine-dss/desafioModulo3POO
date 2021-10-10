29667a413468eccdbb6afc5bb463a8cc7cae2ff5
Elaine <elaine.silva@zup.com.br>
2021-10-10 16:32:54 -0300

    "criar subclasse vendedor"

---
+++ src/entities/Vendedor.java
@@ -0,0 +1,17 @@
+package entities;
+
+public class Vendedor extends Pessoa {
+
+    public Vendedor() {
+        super();
+    }
+
+    public Vendedor(String nome, String cpf, String email) {
+        super(nome, cpf, email);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
